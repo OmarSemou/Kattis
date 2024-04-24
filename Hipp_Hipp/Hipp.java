00a7bbdc9a74326887888b9bfa379b45370ed384
OmarSemou <omse@itu.dk>
2024-04-24 17:40:47 +0200

    Hipp solution

---
+++ Hipp_Hipp/Hipp.java
@@ -0,0 +1,9 @@
+package Hipp_Hipp;
+
+public class Hipp {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Hipp hipp hurra!");
+        }
+    }
+}
